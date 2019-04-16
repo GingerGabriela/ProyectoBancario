b4caa30980bd15fec9b637ff393f427ec26072f9
TantanFpm <taniafpm@riseup.net>
2019-04-16 11:55:25 +0200

    preparado para los test cases pero sin completarlos

---
+++ LosBancos/src/dominio/CuentaTest.java
@@ -0,0 +1,39 @@
+package dominio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CuentaTest {
+
+	@Test
+	void testIngresarDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetirarDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIngresarStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetirarStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSaldo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddMovimiento() {
+		fail("Not yet implemented");
+	}
+
+}
